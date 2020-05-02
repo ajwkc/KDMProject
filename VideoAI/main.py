74bff66a81d73fc2e9836ecf686822a5ee8d8649
ajwkc <andrewwilson88@gmail.com>
2020-05-01 23:31:31 -0500

    Script complete

--- VideoAI/main.py
+++ VideoAI/main.py
@@ -1,4 +1,5 @@
 import argparse
+import matplotlib.pyplot as plt
 
 from google.cloud import videointelligence
 
@@ -13,24 +14,35 @@ def analyze_labels(path):
     result = operation.result(timeout=90)
     print('\nFinished processing.')
 
-    segment_labels = result.annotation_results[0].segment_label_annotations
-    for i, segment_label in enumerate(segment_labels):
-        print('Video label description: {}'.format(
-            segment_label.entity.description))
-        for category_entity in segment_label.category_entities:
-            print('\tLabel category description: {}'.format(
-                category_entity.description))
+    summary = []
 
-        for i, segment in enumerate(segment_label.segments):
+    shot_labels = result.annotation_results[0].shot_label_annotations
+    segment_labels = result.annotation_results[0].segment_label_annotations
+
+    for i, shot_label in enumerate(shot_labels):
+        label = shot_label.entity.description
+        length = 0
+        for j, segment in enumerate(shot_label.segments):
             start_time = (segment.segment.start_time_offset.seconds +
                           segment.segment.start_time_offset.nanos / 1e9)
             end_time = (segment.segment.end_time_offset.seconds +
                         segment.segment.end_time_offset.nanos / 1e9)
-            positions = '{}s to {}s'.format(start_time, end_time)
-            confidence = segment.confidence
-            print('\tSegment {}: {}'.format(i, positions))
-            print('\tConfidence: {}'.format(confidence))
-        print('\n')
+            duration = end_time - start_time
+            length += duration
+        summary.append([label, round(length, 2)])
+
+    summary = sorted(summary, key=lambda x: x[1], reverse=True)
+    summary = summary[:10]
+
+    labels = [i[0] for i in summary]
+    values = [i[1] for i in summary]
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(values, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=180)
+    fig1.suptitle('Top 10 subjects', fontsize=16)
+
+    plt.show()
 
 
 if __name__ == '__main__':
